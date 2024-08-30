f7c23aaabd706b33b1768e16066145616fd8935d
Manoj Dey <71746924+ManojDey224@users.noreply.github.com>
2024-08-30 14:40:19 +0530

    Update mental_health.py

--- mental_health.py
+++ mental_health.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1ExfLpdg3HluUoqzwB7oZtDVbaTpx5tmg
 """
 
-# Commented out IPython magic to ensure Python compatibility.
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -57,7 +57,6 @@ df.status.value_counts()
 
 random_statements = df.groupby('status')['statement'].apply(lambda x: x.sample(n=1).iloc[0])
 
-# Print the results
 for status, statement in random_statements.items():
     print(f"Status: {status}")
     print(f"Statement: {statement}\n")
@@ -66,14 +65,14 @@ for status, statement in random_statements.items():
 import nltk
 nltk.download('punkt')
 
-# Calculate the number of characters and sentences
+
 df['num_of_characters'] = df['statement'].str.len()
 df['num_of_sentences'] = df['statement'].apply(lambda x: len(nltk.sent_tokenize(x)))
 
-# Generate descriptive statistics
+
 description = df[['num_of_characters', 'num_of_sentences']].describe()
 
-# Display the descriptive statistics
+
 print(description)
 
 df[df['num_of_characters'] > 10000]
@@ -100,7 +99,7 @@ def remove_patterns(text):
     text = re.sub(r'[^\w\s]', '', text)
     return text.strip()
 
-# Apply the function to the 'statement' column
+
 df['statement'] = df['statement'].apply(remove_patterns)
 df.head()
 
@@ -109,31 +108,28 @@ df.head()
 
 stemmer = PorterStemmer()
 
-# Function to stem tokens and convert them to strings
+
 def stem_tokens(tokens):
     return ' '.join(stemmer.stem(str(token)) for token in tokens)
 
-# Apply the function to the 'tokens' column
 df['tokens_stemmed'] = df['tokens'].apply(stem_tokens)
 
 df.head()
 
-# Get unique categories in 'status'
 statuses = df['status'].unique()
 
-# Define a color function
+
 def color_func(word, font_size, position, orientation, random_state=101, **kwargs):
     return random.choice(colors)
 
-# Generate and plot the WordCloud for each category
+
 for status in statuses:
     # Filter the tokens data for the current status
     tokens_data = ' '.join(df[df['status'] == status]['tokens'].dropna().apply(lambda x: ' '.join(x)).tolist())
 
-    # Generate the WordCloud
     wordcloud = WordCloud(width=800, height=400, background_color='white', color_func=color_func).generate(tokens_data)
 
-    # Plot the WordCloud
+
     plt.figure(figsize=(10, 5))
     plt.imshow(wordcloud, interpolation='bilinear')
     plt.axis('off')  # Turn off axis
@@ -148,16 +144,14 @@ y = lbl_enc.fit_transform(y.values)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=101)
 
-# 1. Initialize TF-IDF Vectorizer and fit/transform on the 'tokens' column
+
 vectorizer = TfidfVectorizer(ngram_range=(1, 2), max_features=50000)
 X_train_tfidf = vectorizer.fit_transform(X_train['tokens_stemmed'])
 X_test_tfidf = vectorizer.transform(X_test['tokens_stemmed'])
 
-# 2. Extract numerical features
 X_train_num = X_train[['num_of_characters', 'num_of_sentences']].values
 X_test_num = X_test[['num_of_characters', 'num_of_sentences']].values
 
-# 3. Combine TF-IDF features with numerical features
 X_train_combined = hstack([X_train_tfidf, X_train_num])
 X_test_combined = hstack([X_test_tfidf, X_test_num])
 
@@ -181,10 +175,8 @@ classifiers = {
 !pip uninstall xgboost
 !pip install xgboost-gpu
 
-# Initialize a list to store accuracy scores for each classifier
 accuracy_scores = []
 
-# Iterate over each classifier and its name in the classifiers dictionary
 for name, clf in classifiers.items():
     clf.fit(X_train_resampled, y_train_resampled)
     y_pred = clf.predict(X_test_combined)
@@ -194,21 +186,16 @@ for name, clf in classifiers.items():
     print("For", name)
     print("Accuracy:", accuracy)
 
-    # Compute the confusion matrix for the predictions
-    # 'lbl_enc.classes_' provides the class labels for the confusion matrix and classification report
     labels = lbl_enc.classes_
     conf_matrix = confusion_matrix(y_test, y_pred)
     print(classification_report(y_test, y_pred, target_names=labels))
 
-    # Plot the confusion matrix using a heatmap
-    # Annotate each cell with the numeric value of the confusion matrix
     sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Greens', xticklabels=labels, yticklabels=labels)
     plt.xlabel('Predicted')  # Label for x-axis
     plt.ylabel('Actual')     # Label for y-axis
     plt.title(f'Confusion Matrix for {name}')  # Title for the heatmap
     plt.show()  # Display the heatmap
 
-    # Append the accuracy score to the list
     accuracy_scores.append(accuracy)
 
 accuracies_df = pd.DataFrame({'Classifier': classifiers.keys(), 'Accuracy': accuracy_scores}).sort_values('Accuracy', ascending=False)
